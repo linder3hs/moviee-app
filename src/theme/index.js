d133a53882de979cdd3e2585ac272df30fc54823
linder3hs <linderhassinger00@gmail.com>
2022-08-08 19:55:31 -0500

    change color

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,12 +1,12 @@
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
-	palette: {
-		primary: {
-			main: "#171515",
-			contrastText: "#fff",
-		},
-	},
+  palette: {
+    primary: {
+      main: "#3a93a9",
+      contrastText: "#fff",
+    },
+  },
 });
 
 export default theme;
